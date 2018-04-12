74f7a1c60b95b4e7c5e9e0f33504e0b23cb9875b
sng <sng@bsdnetworks.net>
2018-04-12 13:52:20 -0500

    creating ex5.py

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Ray Percival'
+my_age = 47 # not a lie
+my_height = 72 # inches
+my_weight = 213 # lbs
+my_eyes = 'Gray'
+my_teeth = 'White'
+my_hair = 'Grey'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Not bad for a middle aged nerd.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teedh are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
